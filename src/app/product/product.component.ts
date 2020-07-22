83a82e76b62f1fde06dec95220a7bc005032ab92
furkanaltaca <furkanaltaca@gmail.com>
2020-07-22 18:13:16 +0300

    Products filter custom pipe added for product. Alertify added.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 
+declare let alertify: any;
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -10,6 +12,7 @@ export class ProductComponent implements OnInit {
   constructor() {}
 
   title = 'Product List';
+  filterText = '';
   products: Product[] = [
     {
       id: 1,
@@ -17,7 +20,8 @@ export class ProductComponent implements OnInit {
       price: 3000,
       categoryId: 1,
       description: 'Dell Latitude',
-      imageUrl: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     },
     {
       id: 2,
@@ -25,7 +29,8 @@ export class ProductComponent implements OnInit {
       price: 200,
       categoryId: 1,
       description: 'Asus',
-      imageUrl: 'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+      imageUrl:
+        'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
     },
     {
       id: 3,
@@ -33,7 +38,8 @@ export class ProductComponent implements OnInit {
       price: 400,
       categoryId: 1,
       description: 'Rampage',
-      imageUrl: 'https://images.unsplash.com/photo-1572916118970-fb5c8a1cb3d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      imageUrl:
+        'https://images.unsplash.com/photo-1572916118970-fb5c8a1cb3d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     },
     {
       id: 4,
@@ -41,7 +47,8 @@ export class ProductComponent implements OnInit {
       price: 100,
       categoryId: 2,
       description: 'DJI',
-      imageUrl: 'https://images.unsplash.com/photo-1564518384646-b72509b610b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      imageUrl:
+        'https://images.unsplash.com/photo-1564518384646-b72509b610b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     },
     {
       id: 5,
@@ -49,7 +56,8 @@ export class ProductComponent implements OnInit {
       price: 1000,
       categoryId: 1,
       description: 'Casper',
-      imageUrl: 'https://images.unsplash.com/photo-1551645120-d70bfe84c826?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      imageUrl:
+        'https://images.unsplash.com/photo-1551645120-d70bfe84c826?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     },
     {
       id: 6,
@@ -57,7 +65,8 @@ export class ProductComponent implements OnInit {
       price: 4000,
       categoryId: 2,
       description: 'Apple',
-      imageUrl: 'https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      imageUrl:
+        'https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     },
     {
       id: 7,
@@ -65,9 +74,14 @@ export class ProductComponent implements OnInit {
       price: 9000,
       categoryId: 1,
       description: 'Apple',
-      imageUrl: 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     },
   ];
 
   ngOnInit(): void {}
+
+  addToCart(product: Product) {
+    alertify.success(product.name + ' sepete eklendi.');
+  }
 }
