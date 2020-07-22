2d0826f6988f8516e41cc3423776313b7d67dbff
furkanaltaca <furkanaltaca@gmail.com>
2020-07-22 21:38:31 +0300

    HttpClientModule added. Services added for HttpClient.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from './category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css'],
+  providers: [CategoryService],
 })
 export class CategoryComponent implements OnInit {
-  constructor() {}
+  constructor(private categoryService: CategoryService) {}
+
   title = 'Category List';
-  categories: Category[] = [
-    { id: 1, name: 'Bilgisayar' },
-    { id: 2, name: 'Elektronik' },
-    { id: 2, name: 'İletişim' },
-    { id: 2, name: 'Müzik' },
-    { id: 2, name: 'Diğer' },
-  ];
-  ngOnInit(): void {}
+  categories: Category[];
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
 }
+
+
