98d7f854a3b72e2056826d3bc93a0b70a78f850e
furkanaltaca <furkanaltaca@gmail.com>
2021-02-18 18:31:19 +0300

    Write code for product-add-reactive component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -23,7 +23,7 @@ import { ProductAddReactiveComponent } from './product/product-add-reactive/prod
     ProductAddClassicComponent,
     ProductAddReactiveComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [AlertifyService],
   bootstrap: [AppComponent],
 })
--- src/app/product/product-add-reactive/product-add-reactive.component.ts
+++ src/app/product/product-add-reactive/product-add-reactive.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/category/category';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-add-reactive',
   templateUrl: './product-add-reactive.component.html',
-  styleUrls: ['./product-add-reactive.component.css']
+  styleUrls: ['./product-add-reactive.component.css'],
+  providers: [CategoryService, ProductService],
 })
 export class ProductAddReactiveComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private alertifyService: AlertifyService
+  ) {}
 
-  constructor() { }
+  productAddForm: FormGroup;
+  product: Product = new Product();
+  categories: Category[];
 
-  ngOnInit(): void {
+  createProductAddForm() {
+    this.productAddForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      imageUrl: ['', Validators.required],
+      price: ['', Validators.required],
+      categoryId: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {
+    this.createProductAddForm();
+
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
+
+  addProduct() {
+    if (this.productAddForm.valid) {
+      this.product = Object.assign({}, this.productAddForm.value);
+    }
+
+    this.productService.addProduct(this.product).subscribe((data) => {
+      this.alertifyService.success(data.name + ' başarıyla eklendi.');
+    });
+  }
 }
