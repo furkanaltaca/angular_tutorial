7acb8853d9835209178a65f8ec18375c6fce4925
furkanaltaca <furkanaltaca@gmail.com>
2021-02-18 03:00:34 +0300

    Edit product create page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './product/product.component';
+import { ProductAddClasicComponent } from './product/product-add-clasic/product-add-clasic.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductComponent },
   { path: 'products/category/:categoryId', component: ProductComponent },
+  { path: 'product-add-clasic', component: ProductAddClasicComponent },
+  { path: 'products-add-reactive', component: ProductComponent },
 ];
 
 @NgModule({
--- src/app/product/product-add-clasic/product-add-clasic.component.ts
+++ src/app/product/product-add-clasic/product-add-clasic.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Category } from 'src/app/category/category';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-add-clasic',
   templateUrl: './product-add-clasic.component.html',
-  styleUrls: ['./product-add-clasic.component.css']
+  styleUrls: ['./product-add-clasic.component.css'],
+  providers: [CategoryService, ProductService],
 })
 export class ProductAddClasicComponent implements OnInit {
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private alertifyService: AlertifyService
+  ) {}
 
-  constructor() { }
+  product: Product = new Product();
+  categories: Category[];
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
   }
 
+  add(form: NgForm) {
+    this.productService.addProduct(this.product).subscribe((data) => {
+      this.alertifyService.success(data.name + " başarıyla eklendi.")
+    });
+  }
 }
